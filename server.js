6a22a7088d5d5e669f2cdf394c3deb894480f69c
Skyler <sjskyler27@gmail.com>
2023-04-22 13:31:19 -0600

    shortend port syntax

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express(); // the object app now has the functionallity of express
 const controller = require('./routes/controllers');
 
 app.use('/', require('./routes/paths'));
-const port = 3000
+const port = process.env.port || 3000;
 
-app.listen(port || process.env.port);
-console.log('Web server at port: ' + (port || process.env.port));
+app.listen(port);
+console.log('Web server at port: ' + port);
 
